127f7a4fd48e57ffc5a51bd7353606608d9d1cf6
solvedDev <solvedDev@gmail.com>
2020-10-26 20:11:05 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { join } from 'https://deno.land/std@0.74.0/path/mod.ts'
 import { iterateDir } from './iterateDir.ts'
 
 if (import.meta.main) {
-	const args = parse(Deno.args, { alias: { folder: 'f' } })
-	console.dir(args)
+	const args = parse(Deno.args)
+	if (!args.bp || !args.rp || !args.rploc || !args.bploc)
+		throw new Error(`Missing argument!`)
 
 	iterateDir(args.bp, args.bploc, join(args.bp, 'bridge/cache/BP'))
 	iterateDir(args.rp, args.rploc, join(args.bp, 'bridge/cache/RP'))
